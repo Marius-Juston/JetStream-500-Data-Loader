0188e038d62dd8e09bfe262a78ec773f49593e20
NamelessSuperCoder <Marius.juston@hotmail.fr>
2018-09-21 18:36:08 -0400

    minor reformating

--- src/dataLoader/Main.java
+++ src/dataLoader/Main.java
@@ -13,7 +13,7 @@ public class Main extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public final void start(Stage primaryStage) throws java.io.IOException {
     Parent root = FXMLLoader.load(getClass().getResource("mainSelector.fxml"));
     primaryStage.setTitle("JetStream 500 Data Loader");
     primaryStage.setScene(new Scene(root));
